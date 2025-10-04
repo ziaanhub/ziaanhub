b3f03f62525d6a0c5e90ecfe5e7763ab5d019c82
ziaanlabs <rfeiammlnee@gmail.com>
2025-10-04 19:38:30 +0700

    Update ziaanhub.lua

--- source/core/ziaanhub.lua
+++ source/core/ziaanhub.lua
@@ -1,5 +1,5 @@
 
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/Core/ZiaanHub2.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ziaanlabs/ziaanhub/refs/heads/main/source/core/ziaancore.lua"))()
 
 local URL = Games[game.PlaceId]
 
