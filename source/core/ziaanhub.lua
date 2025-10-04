4628c4a4c64520050694e58593386333a408422a
ziaanlabs <rfeiammlnee@gmail.com>
2025-10-04 19:28:53 +0700

    Update ziaanhub.lua

--- source/core/ziaanhub.lua
+++ source/core/ziaanhub.lua
@@ -1,1 +1,11 @@
 
+local Games = {
+  [82321750197896] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/Games/PoopABrainrot/mnc.lua", -- PoopABrainRot
+  [113604074601559] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/Games/BuildABeehive/cmn.lua",  -- "Build a Beehive
+  [2753915549] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 3
+  
+}
+
+return Games
