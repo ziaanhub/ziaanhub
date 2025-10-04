08dfedc839f3c90944a1486b2854a12993bbdcb0
ziaanhub <frmlnrmbee@gmail.com>
2025-10-04 21:32:21 +0700

    Update ziaancore.lua

--- source/core/ziaancore.lua
+++ source/core/ziaancore.lua
@@ -1,6 +1,6 @@
 
 local Games = {
-  [82321750197896] = "https://raw.githubusercontent.com/ziaanlabs/ziaanhub/refs/heads/main/source/default/A/Games/PBB/csx.lua", -- PoopABrainRot
+  [82321750197896] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/Games/PBB/csx.lua", -- PoopABrainRot
   [113604074601559] = "https://raw.githubusercontent.com/ziaanlabs/ziaanhub/refs/heads/main/source/default/A/B/Games/BAH/css.lua",  -- "Build a Beehive
   [2753915549] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 2
