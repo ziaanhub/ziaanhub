59a14355217cc5f0cda1a1910f6a31cb46206a1c
ziaanlabs <rfeiammlnee@gmail.com>
2025-10-04 19:37:56 +0700

    Update ziaanhub.lua

--- source/core/ziaanhub.lua
+++ source/core/ziaanhub.lua
@@ -1,11 +1,10 @@
 
-local Games = {
-  [82321750197896] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/Games/PoopABrainrot/mnc.lua", -- PoopABrainRot
-  [113604074601559] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/Games/BuildABeehive/cmn.lua",  -- "Build a Beehive
-  [2753915549] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/a/b/c/Games/BloxFruit/Bf.lua.txt", -- Blox Fruit Sea 3
-  
-}
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/Core/ZiaanHub2.lua"))()
 
-return Games
+local URL = Games[game.PlaceId]
+
+if URL then
+    loadstring(game:HttpGet(URL))()
+else
+    warn("Error Bro")
+end
