b19bc9d704246fad78c1ab42bb602a3a33115022
ziaanlabs <rfeiammlnee@gmail.com>
2025-10-04 19:25:28 +0700

    Update ziaancore.lua

--- source/core/ziaancore.lua
+++ source/core/ziaancore.lua
@@ -1,1 +1,10 @@
 
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ziaandev/ZiaanHub/refs/heads/main/Source/Core/ZiaanHub2.lua"))()
+
+local URL = Games[game.PlaceId]
+
+if URL then
+    loadstring(game:HttpGet(URL))()
+else
+    warn("Error Bro")
+end
