3f4689a4892a2ac6f74749fbe32982ab8c2a1680
ziaandev <frmlnrmbee@gmail.com>
2025-10-05 21:55:03 +0700

    Update ziaancore.lua

--- source/core/ziaancore.lua
+++ source/core/ziaancore.lua
@@ -2,9 +2,9 @@
 local Games = {
   [82321750197896] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/Games/PBB/csx.lua", -- PoopABrainRot
   [113604074601559] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/Games/BAH/css.lua",  -- "Build a Beehive
-  [2753915549] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/Bf.lua.txt", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/Bf.lua.txt", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/Bf.lua.txt", -- Blox Fruit Sea 3
+  [2753915549] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 3
   
 }
 
