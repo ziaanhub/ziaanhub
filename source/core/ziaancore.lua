f212d272420095c7e92b5df12107fdbe1c4a0ea3
ziaandev <frmlnrmbee@gmail.com>
2025-10-06 00:34:38 +0700

    Update ziaancore.lua

--- source/core/ziaancore.lua
+++ source/core/ziaancore.lua
@@ -4,16 +4,15 @@ local Games = {
   [113604074601559] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/Games/BAH/css.lua",  -- "Build a Beehive
   [2753915549] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 3
-  
-  [10324346056] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/Games/PBB/csx.lua", -- evade
-  [9872472334] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/Games/BAH/css.lua",  -- evade
-  [10662542523] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- evade
-  [10324347967] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- evade
-  [121271605799901] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- evade
-  [10808838353] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/Games/PBB/csx.lua", -- evade
-  [11353528705] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/Games/BAH/css.lua",  -- evade
-  [99214917572799] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- evade
+  [7449423635] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 3  
+  [10324346056] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade
+  [9872472334] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua",  -- evade
+  [10662542523] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade
+  [10324347967] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade
+  [121271605799901] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade
+  [10808838353] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade
+  [11353528705] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua",  -- evade
+  [99214917572799] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade
 
 }
 
