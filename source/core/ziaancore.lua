bcee6c1c6fa593d047fbf54428a0310d38d650a9
ziaandev <frmlnrmbee@gmail.com>
2025-10-12 18:36:37 +0700

    Update ziaancore.lua

--- source/core/ziaancore.lua
+++ source/core/ziaancore.lua
@@ -5,16 +5,16 @@ local Games = {
   [2753915549] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 2
   [7449423635] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/csp.lua", -- Blox Fruit Sea 3  
-  [10324346056] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade Big Team
-  [9872472334] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua",  -- evade Evade
-  [10662542523] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade Casual
-  [10324347967] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade Social Space
-  [121271605799901] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade Player Nextbots
-  [10808838353] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade VC Only
-  [11353528705] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua",  -- evade Pro
-  [99214917572799] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/EVD/csx.lua", -- evade Custom Servers
-  [12334109280] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/C/Games/GBD/vsp.lua", -- Guns & Black Powder
-  [14216737767] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/C/Games/GBD/vsp.lua", -- Guns & Black Powder Mobile
+  [10324346056] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Big Team
+  [9872472334] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua",  -- evade Evade
+  [10662542523] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Casual
+  [10324347967] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Social Space
+  [121271605799901] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Player Nextbots
+  [10808838353] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade VC Only
+  [11353528705] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua",  -- evade Pro
+  [99214917572799] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Custom Servers
+  [12334109280] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- Guns & Black Powder
+  [14216737767] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- Guns & Black Powder Mobile
 }
 
 return Games
